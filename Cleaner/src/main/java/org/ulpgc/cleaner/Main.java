fa1bd181f4cd45766684c6e307dcfb42af196204
marap <marapareja17@gmail.com>
2023-11-03 16:01:50 +0000

    v1.0.1

--- Cleaner/src/main/java/org/ulpgc/cleaner/Main.java
+++ Cleaner/src/main/java/org/ulpgc/cleaner/Main.java
@@ -1,9 +1,10 @@
 package org.ulpgc.cleaner;
 
-import org.ulpgc.cleaner.controller.Controller;
+import org.ulpgc.cleaner.view.APIController;
 
 public class Main {
     public static void main(String[] args) {
-        Controller.run(args[0]);
+        APIController apiController = new APIController();
+        apiController.run();
     }
 }
--- Cleaner/src/main/java/org/ulpgc/cleaner/controller/reader/Reader.java
+++ Cleaner/src/main/java/org/ulpgc/cleaner/controller/reader/Reader.java
@@ -4,6 +4,7 @@ import java.io.*;
 import java.nio.file.Path;
 
 public class Reader {
+
     public String readFile(Path filePath) throws IOException {
         StringBuilder content = new StringBuilder();
 
@@ -17,4 +18,32 @@ public class Reader {
         }
         return content.toString();
     }
+
+    public String getMetadata(String idBook) {
+        Path filePath = Path.of("datalake/metadata/" + idBook);
+        try {
+            return readFile(filePath);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public String getContent(String idBook) {
+        Path filePath = Path.of("datalake/content/" + idBook);
+        try {
+            return readFile(filePath);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public String getRawBook(String idBook) {
+        Path filePath = Path.of("datalake/books/" + idBook);
+        try {
+            return readFile(filePath);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
 }
