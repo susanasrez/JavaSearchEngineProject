18deb945ce424a60c31db130302a16a88c54561e
Ricardo Cárdenes <78762064+ricardocardn@users.noreply.github.com>
2023-11-03 15:33:19 +0100

    v1.0.0

--- Cleaner/src/main/java/org/ulpgc/cleaner/controller/process/ParseFiles.java
+++ Cleaner/src/main/java/org/ulpgc/cleaner/controller/process/ParseFiles.java
@@ -6,8 +6,8 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class ParseFiles {
-    public List<String> processFile(String content, String name) {
-        List<String> contentSplit = new ArrayList<String>();
+    public List<String> processFile(String content) {
+        List<String> contentSplit = new ArrayList<>();
         String[] parts = content.split("\\*\\*\\*");
 
         String metadata = parts[0] + "***" + parts[1] + "***" + "\n" + "***" + parts[3] + "***" + parts[4];
@@ -20,7 +20,7 @@ public class ParseFiles {
     }
 
     public static String renameFile(String filename) {
-        Pattern pattern = Pattern.compile("_(\\d+)_");
+        Pattern pattern = Pattern.compile("(\\d+).txt");
         Matcher matcher = pattern.matcher(filename);
 
         if (matcher.find()) {
